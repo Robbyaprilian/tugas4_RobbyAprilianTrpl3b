1759b98a56d7a6fa737f6f52e9422fc8ab6da6d8
Robbyaprilian <75844276+Robbyaprilian@users.noreply.github.com>
2020-12-19 10:26:40 +0700

    Add files via upload

---
+++ tugas4RobbyAprilianTrpl3b-main/tugas4RobbyAprilianTrpl3b-main/tugas4RobbyAprilianTrpl3b/Framework/git/order/services/handler_fastpay.go
@@ -0,0 +1,14 @@
+package services
+
+import (
+	"context"
+
+	cm "tugas4RobbyAprilianTrpl3b/Framework/git/order/common"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func (PaymentService) FastPayHandler(ctx context.Context, req cm.FastPayRequest) (res cm.FastPayResponse) {
+
+	return
+}
